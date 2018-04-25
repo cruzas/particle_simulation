d07677b34263ac2ec0c73b0441b5034e0542b0c2
stud04 <stud04@icsnode09.cluster.net>
2018-04-25 16:43:37 +0200

    Changed makefile and added kernel to particle.cpp

---
+++ code/cpp_version/particles.cpp
@@ -0,0 +1,407 @@
+/**
+* Authors: Samuel A. Cruz Alegría, Alessandra M. de Felice, Hrishikesh R. Gupta.
+*
+* This program simulates particle movement in 2D space.
+*/
+
+#include <algorithm>
+#include <ctime>
+#include <cstring>
+#include <chrono>
+#include <fstream>
+#include <iostream>
+#include <cstdlib>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>       /* clock_t, clock, CLOCKS_PER_SEC */
+#include <vector>
+
+#include "particles.h"
+
+using namespace std;
+using namespace std::chrono; // For timing.
+
+#define DEFAULT_WIDTH 800
+#define DEFAULT_HEIGHT 800
+#define DEFAULT_DEPTH 800
+
+#define DEBUGGING 1
+#undef DEBUGGING
+
+static const string PDPATH = "./particle_positions/";  // Path to write files to.
+static int use_openacc;     // Whether we use OpenACC or not.
+static int width;         // Width of box containing particles.
+static int height;        // Height of box containing particles.
+static int depth;         // Depth of box containing particles.
+static int n;             // Number of particles.
+static float fx;          // Horizontal component of the force field.
+static float fy;          // Vertical component of the force field.
+static float fz;          // Depth component of the force field.
+static float radius;      // Radius of the particles, in pixels.
+static float delta;       // Time, in seconds, for inter-frame interval.
+static int total_time_interval;    // Time, in seconds, for total time interval.
+static float g;           // Gravitational factor (in y direction).
+
+vector<float> pxvec;      // Vector of particle x positions.
+vector<float> pyvec;      // Vector of particle y positions.
+vector<float> pzvec;      // Vector of particle z positions.
+
+vector<float> vxvec;      // Vector of particle velocity x components.
+vector<float> vyvec;      // Vectory of particle velocity y components.
+vector<float> vzvec;      // Vector of particle velocity z components.
+
+void print_all_particle_details();
+int write_all_particle_details_to_file(string filename);
+
+/*
+* Print expected usage of this program.
+*/
+void
+print_usage()
+{
+  cerr << "Usage: << [use_openacc=1 or 0] "
+       << "[width=box_width] "
+       << "[height=box_height] "
+       << "[n=num_particles] "
+       << "[fx=forcefield_x] "
+       << "[fy=forcefield_y] "
+       << "[fz=forcefield_z]"
+       << "[trace=shading_factor_trace] "
+       << "[radius=particle_radius] "
+       << "[delta=inter_frame_interval_in_seconds] "
+       << "[total_time_interval=total_time_in_seconds]\n";
+}
+
+
+// main() is where program execution begins.
+int
+main(int argc, char *argv[])
+{
+  // Do all necessary initializations.
+  if (!init_params(argc, argv) || !init_particles()) {
+    return -1;
+  }
+
+  // double avg_cpu_time_used = 0;
+  struct timespec t1;
+  struct timespec t2;
+
+  double avg_cpu_time = 0;
+
+  // Calculate number of loop cycles to be performed given a total time interval
+  // and time per frame.
+  int nCycles = total_time_interval / delta;
+  for (int cycle = 0; cycle < nCycles; ++cycle) {
+    float current_time_frame = delta * cycle;
+
+    string cur_timeframe_str = to_string(current_time_frame);
+
+    // Replace '.' in number.
+    // cur_timeframe_str.erase(std::remove(cur_timeframe_str.begin(), cur_timeframe_str.end(), '.'), cur_timeframe_str.end());
+    string::size_type start = 0;
+    string dot = ".";
+    start = cur_timeframe_str.find(dot, start);
+
+    char * cstr = new char [cur_timeframe_str.length()+1];
+    std::strcpy (cstr, cur_timeframe_str.c_str());
+
+    memmove(&cstr[start], &cstr[start + 1], strlen(cstr) - start);
+    string actual_string(cstr);
+    delete [] cstr;
+
+    string filename ("positions_" + actual_string + ".vtk");
+
+    // Call function to update particle details and time it.
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &t1);
+    update_particles();
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &t2);
+
+    // Add current duration to average, to be later divided by number of cycles,
+    // which is the number of times update_particles() is called.
+    avg_cpu_time += t2.tv_nsec-t1.tv_nsec ;
+
+    if (!write_all_particle_details_to_file(filename)) {
+      cerr << "Could not write file: " << filename << "\n";
+      return -1;
+    }
+  }
+
+  // Calculate average duration.
+  avg_cpu_time /= nCycles;
+  cout << "avg_cpu_time=" << avg_cpu_time << "\n";
+
+  return 0;
+}
+
+/*
+* Initialize all particle details.
+* @return 1 on success, 0 on error.
+*/
+int
+init_particles()
+{
+  // Allocate space for particle positions.
+  pxvec.reserve(n);
+  pyvec.reserve(n);
+  pzvec.reserve(n);
+  // Allocate space for particle velocities.
+  vxvec.reserve(n);
+  vyvec.reserve(n);
+  vzvec.reserve(n);
+
+  /* The srand() function sets its argument seed as the seed for a new
+   * sequence of pseudo-random numbers to be returned by rand().  These
+   * sequences are repeatable by calling srand() with the same seed value.
+   *
+   *
+   * time(0) explanation from: https://stackoverflow.com/questions/4736485/srandtime0-and-random-number-generation
+   * time(0) gives the time in seconds since the Unix epoch, which is a
+   * pretty good "unpredictable" seed (you're guaranteed your seed will be the
+   * same only once, unless you start your program multiple times within the
+   * same second).*/
+  srand(time(0));
+
+  // Go through all particles and initialize their details at random.
+//#pragma acc kernels
+  for (int id = 0; id < n; ++id) {
+    // Set x position.
+    pxvec.push_back((float) (rand() % DEFAULT_WIDTH));
+    // Set y position.
+    pyvec.push_back((float) (rand() % DEFAULT_HEIGHT));
+    // Set z position.
+    pzvec.push_back((float) (rand() % DEFAULT_DEPTH));
+    // Set velocity x-component.
+    vxvec.push_back(rand() / (float) RAND_MAX * fx);
+    // Set velocity y-component.
+    vyvec.push_back(rand() / (float) RAND_MAX * fy);
+    // Set velocity z-component.
+    vzvec.push_back(rand() / (float) RAND_MAX * fz);
+
+    // Correct starting x position.
+    if (pxvec[id] - radius <= 0) {
+      pxvec[id] = radius;
+    } else if (pxvec[id] + radius >= DEFAULT_WIDTH) {
+      pxvec[id] = DEFAULT_WIDTH - radius;
+    }
+
+    // Correct starting y position.
+    if (pyvec[id] - radius <= 0) {
+      pyvec[id] = radius;
+    } else if (pyvec[id] + radius >= DEFAULT_HEIGHT) {
+      pyvec[id] = DEFAULT_HEIGHT - radius;
+    }
+
+    // Correct starting z position.
+    if (pzvec[id] - radius <= 0) {
+      pzvec[id] = radius;
+    } else if (pzvec[id] + radius >= DEFAULT_DEPTH) {
+      pzvec[id] = DEFAULT_DEPTH - radius;
+    }
+  }
+
+  return 1;
+}
+
+
+/*
+* Update the particle details.
+* @return 1 on success, 0 on error.
+*/
+int
+update_particles()
+{
+  // Loop through all particles
+#pragma acc kernels  
+for (int id = 0; id < n; ++id) {
+    float px = pxvec[id];  // x position.
+    float py = pyvec[id];  // y position.
+    float pz = pzvec[id];  // z position.
+    float vx = vxvec[id];  // velocity x-component.
+    float vy = vyvec[id];  // velocity y-component.
+    float vz = vzvec[id];  // velocity z-component.
+
+    // Set new x direction based on horizontal collision with wall.
+    if (px + radius >= width || px - radius <= 0) {
+      vx = vx * -1;
+    }
+
+    // Set new y direction based on vertical collision with wall.
+    if (py - radius <= 0 || py + radius >= height) {
+      vy = vy * -1;
+    }
+
+    // Set new z direction based on depth collision with wall.
+    if (pz + radius >= depth || pz - radius <= 0) {
+      vz = vz * -1;
+    }
+
+    // Update particle position.
+    pxvec[id] = px + (vx*delta);
+    pyvec[id] = py - (vy*delta) - (0.5 * g * delta * delta);
+    pzvec[id] = pz + (vz*delta);
+
+    // Update velocity components.
+    vxvec[id] = vx;  // vx only as acceleration in x-direction is 0.
+    vyvec[id] = vy + 0.5*(g)*delta;
+    vzvec[id] = vz; // vz only as acceleration in z-direction is 0.
+
+    // Correct x position in case updated position goes past wall.
+    if (pxvec[id] - radius <= 0) {
+      pxvec[id] = radius;
+    } else if (pxvec[id] + radius >= height) {
+      pxvec[id] = width - radius;
+    }
+
+    // Correct y position in case updated position goes past wall.
+    if (pyvec[id] - radius <= 0) {
+      pyvec[id] = radius;
+    } else if (pyvec[id] + radius >= height) {
+      pyvec[id] = height - radius;
+    }
+
+    // Correct z position in case updated position goes past wall.
+    if (pzvec[id] - radius <= 0) {
+      pzvec[id] = radius;
+    } else if (pzvec[id] + radius >= depth) {
+      pzvec[id] = depth - radius;
+    }
+  }
+
+  return 1;
+}
+
+/* Print the details of all particles.*/
+void
+print_all_particle_details()
+{
+  for (int i = 0; i < n; ++i) {
+    cout << "particles" << i
+    << ": "
+    << "px=" << pxvec[i] << ", "
+    << "py=" << pyvec[i] << ", "
+    << "pz=" << pzvec[i] << ", "
+    << "vx=" << vxvec[i] << ", "
+    << "vy=" << vyvec[i] << ", "
+    << "vz=" << vzvec[i] << "\n";
+  }
+}
+
+/* Write the details of all particles to file with given filename.
+* The path PDPATH is used to specify where to save the file.
+* @return 1 on success, 0 on failure.*/
+int
+write_all_particle_details_to_file(string filename)
+{
+  ofstream myfile;
+
+  myfile.open(PDPATH + filename, ios::out);
+  if (myfile.is_open()) {
+
+    myfile << "# vtk DataFile Version 1.0\n";
+    myfile << "3D triangulation data\n";
+    myfile << "ASCII\n\n";
+
+    myfile << "DATASET POLYDATA\n";
+    myfile << "POINTS " << n << " float\n";
+
+    for (int i = 0; i < n; ++i) {
+      // Write particle i's position to file.
+      myfile << pxvec[i] << " " << pyvec[i] << " " << pzvec[i] << "\n";
+    }
+
+    myfile.close();
+  } else {
+    cerr << "Unable to open file: " << filename << "\n";
+    return 0;
+  }
+
+  return 1;
+}
+
+/* Initialize default parameters.
+* @return 1 on success, 0 on failure.*/
+int
+init_params(int argc, char *argv[])
+{
+
+  use_openacc = 0;          // By default, do not use OpenACC.
+  width = DEFAULT_WIDTH;    // Width of box containing particles.
+  height = DEFAULT_HEIGHT;  // Height of box containing particles.
+  depth = DEFAULT_DEPTH;    // Depth of box containing particles.
+  n = 5;                    // Number of particles.
+  fx = 50;                  // Horizontal component of the force field.
+  fy = 50;                  // Vertical component of the force field.
+  fz = 50;                  // Depth component of the force field.
+  radius = 5;               // Radius of the particles, in pixels.
+  delta = 1.0;              // Time, in seconds, for inter-frame interval.
+  total_time_interval = 10; // Time, in seconds, for total time interval.
+  g = -9.8;                 // Gravitational factor (in y direction).
+
+
+  // Read and process command-line arguments.
+  for (int i = 1; i < argc; ++i) {
+    if(!process_arg(argv[i])) {
+      cerr << "Invalid argument: " << argv[i] << "\n";
+      print_usage();
+      return 0;
+    }
+  }
+
+  #ifdef DEBUGGING
+  printf("use_openacc=%d\n", use_openacc);
+  printf("width=%d\n", width);
+  printf("height=%d\n", height);
+  printf("n=%d\n", n);
+  printf("fx=%f\n", fx);
+  printf("fy=%f\n", fy);
+  printf("fz=%f\n", fz);
+  printf("radius=%f\n", radius);
+  printf("delta=%f\n", delta);
+  printf("total_time_interval=%d\n", total_time_interval);
+  printf("g: %f\n", g);
+  #endif
+
+  return 1;
+}
+
+
+/*
+* Process the given command-line parameter.
+* @param arg The command-line parameter.
+* @return 1 on success, 0 on error.*/
+int
+process_arg(char *arg)
+{
+  if (strstr(arg, "use_openacc="))
+  return sscanf(arg, "use_openacc=%d", &use_openacc) == 1;
+
+  else if (strstr(arg, "width="))
+  return sscanf(arg, "width=%d", &width) == 1;
+
+  else if (strstr(arg, "height="))
+  return sscanf(arg, "height=%d", &height) == 1;
+
+  else if (strstr(arg, "n="))
+  return sscanf(arg, "n=%d", &n) == 1;
+
+  else if (strstr(arg, "fx="))
+  return sscanf(arg, "fx=%f", &fx) == 1;
+
+  else if (strstr(arg, "fy="))
+  return sscanf(arg, "fy=%f", &fy) == 1;
+
+  else if (strstr(arg, "fz="))
+  return sscanf(arg, "fz=%f", &fy) == 1;
+
+  else if (strstr(arg, "radius="))
+  return sscanf(arg, "radius=%f", &radius) == 1;
+
+  else if (strstr(arg, "delta="))
+  return sscanf(arg, "delta=%f", &delta) == 1;
+
+  else if (strstr(arg, "total_time_interval="))
+  return sscanf(arg, "total_time_interval=%d", &total_time_interval) == 1;
+
+  // Return 0 if the given command-line parameter was invalid.
+  return 0;
+}
