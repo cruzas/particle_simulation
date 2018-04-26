a02f279927b3cb688a792d50743eb346a7b1515f
stud04 <stud04@icsnode09.cluster.net>
2018-04-26 14:27:32 +0200

    Added openacc benchmark

--- code/cpp_version/benchmark_collect.sh
+++ code/cpp_version/benchmark_collect.sh
@@ -28,5 +28,26 @@ do
 done
 
 # TODO: Execute code using OpenACC.
+# declare -a nParticles=(10 100 1000 10000)
+
+pos=$(( ${#nParticles[*]} - 1 ))
+last=${nParticles[$pos]}
+
+filename_parallel="benchmark_parallel.txt"
+
+echo "num_particles" "," "time_ns" > $filename_parallel
+
+# TODO: Add up average times.
+
+for i in "${nParticles[@]}"
+do
+  iParticles=$i
+  # echo "nParticles="$i >> $filename_serial
+  # TODO: fix so that avg_duration is added to line that says nParticles=...
+  avg_time=`./particles_parallel n=$i delta=0.5 total_time_interval=100 | cut -d= -f2`
+
+  echo $iParticles "," $avg_time >> $filename_parallel
+
+  rm particle_positions/positions_*.vtk
 
 echo All done
