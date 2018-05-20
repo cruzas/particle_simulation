3c8db3db7cb5ec93b99567fd6740c70d8486bfe7
stud03 <stud03@icsnode09.cluster.net>
2018-05-20 17:46:52 +0200

    test_machine_eps now also prints size of double

--- test_machine_eps/test.cpp
+++ test_machine_eps/test.cpp
@@ -8,6 +8,8 @@ int
 main(int argc, char *argv[])
 {
 	std::cout << "machine_eps:" << std::numeric_limits<double>::epsilon() << "\n";
+	std::cout << "Size of double: " << sizeof(double) << " bytes" << "\n";
+
 	return 0;
 }
 
